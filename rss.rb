3e4fc723db6c3fbf72299a97cfdf73ce1bba6825
Robin Smith <robin@mrrobinsmith.com>
2015-05-13 15:30:22 +0100

    Colorized

--- rss.rb
+++ rss.rb
@@ -6,15 +6,22 @@ require 'colorize'
 
 url = ARGV[0]
 
+FeedLimit = 10
+
 open(url) do |rss|
   feed = RSS::Parser.parse(rss)
-  puts "Title: ".cyan << "#{feed.channel.title}\n".white
+  feed.channel.title.length.times { printf '-'.white }; puts
+  printf "#{feed.channel.title}\n".cyan
+  feed.channel.title.length.times { printf '-'.white }; puts; puts
   feed.items.each_with_index do |item, index|
-    puts "#{item.title}".green
-    puts "#{item.link}".yellow
-    if (index < (feed.items.length - 1))
+    if (index < FeedLimit)
+      then
+      puts "#{item.title}".white
+      puts "#{item.link}".yellow
+      if ((index < (feed.items.length - 1)) && (index < (FeedLimit - 1)))
       then
         puts
+      end
     end
   end
 end
